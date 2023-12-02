694def069fb7e2126b73cd148c2701c4196de993
Demian Ferreiro <epidemian@gmail.com>
2023-12-02 16:59:37 -0300

    Rename some things and extract little function

--- src/bin/day_01_trebuchet.rs
+++ src/bin/day_01_trebuchet.rs
@@ -8,23 +8,23 @@ fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn calibration_value(line: &str) -> Option<u32> {
-    let digits: Vec<_> = line.chars().filter_map(|ch| ch.to_digit(10)).collect();
+fn calibration_value(s: &str) -> Option<u32> {
+    let digits: Vec<_> = s.chars().filter_map(|ch| ch.to_digit(10)).collect();
     Some(digits.first()? * 10 + digits.last()?)
 }
 
-fn calibration_value_p2(line: &str) -> Option<u32> {
+fn calibration_value_p2(s: &str) -> Option<u32> {
+    let digits: Vec<_> = s
+        .char_indices()
+        .filter_map(|(i, ch)| ch.to_digit(10).or_else(|| word_digit_at(s, i)))
+        .collect();
+    Some(digits.first()? * 10 + digits.last()?)
+}
+
+fn word_digit_at(s: &str, pos: usize) -> Option<u32> {
     let num_words = [
         "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
     ];
-    let digits: Vec<_> = line
-        .char_indices()
-        .filter_map(|(i, ch)| {
-            ch.to_digit(10).or_else(|| {
-                let p = num_words.iter().position(|w| line[i..].starts_with(w))?;
-                Some((p + 1) as u32)
-            })
-        })
-        .collect();
-    Some(digits.first()? * 10 + digits.last()?)
+    let d = num_words.iter().position(|w| s[pos..].starts_with(w))? + 1;
+    Some(d as u32)
 }
