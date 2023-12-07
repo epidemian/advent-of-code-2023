264599158c8aee360acfd0542ad47c2bcaa5899f
Demian Ferreiro <epidemian@gmail.com>
2023-12-07 19:11:57 -0300

    Add day 7 part 2 solution

--- src/bin/day_07_camel_cards.rs
+++ src/bin/day_07_camel_cards.rs
@@ -4,14 +4,23 @@ use std::cmp::Ordering;
 fn main() -> aoc::Result<()> {
     let input = aoc::read_stdin()?;
     let mut hands: Vec<_> = input.lines().map(parse_line).try_collect()?;
+
     hands.sort_by_key(|(hand, _bid)| *hand);
-    let ans_1: u64 = hands
+    let ans_1 = total_winnings(&hands);
+
+    hands.sort_by(|a, b| a.0.cmp_with_jokers(&b.0));
+    let ans_2 = total_winnings(&hands);
+
+    println!("{ans_1} {ans_2}");
+    Ok(())
+}
+
+fn total_winnings(hands: &[(Hand, u64)]) -> u64 {
+    hands
         .iter()
         .enumerate()
-        .map(|(i, (_hand, bid))| *bid * (i + 1) as u64)
-        .sum();
-    println!("{ans_1}");
-    Ok(())
+        .map(|(i, &(_hand, bid))| bid * (i + 1) as u64)
+        .sum()
 }
 
 #[derive(Eq, PartialEq, Clone, Copy)]
@@ -30,8 +39,22 @@ enum HandType {
     FiveOfAKind,
 }
 
+impl Ord for Hand {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.hand_type()
+            .cmp(&other.hand_type())
+            .then_with(|| self.cards.map(card_value).cmp(&other.cards.map(card_value)))
+    }
+}
+
+impl PartialOrd for Hand {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
 impl Hand {
-    fn type_(&self) -> HandType {
+    fn hand_type(&self) -> HandType {
         let mut counts: Vec<_> = self.cards.into_iter().counts().into_values().collect();
         counts.sort();
         match counts[..] {
@@ -45,25 +68,46 @@ impl Hand {
             _ => unreachable!(),
         }
     }
-}
 
-impl Ord for Hand {
-    fn cmp(&self, other: &Self) -> Ordering {
-        self.type_()
-            .cmp(&other.type_())
-            .then_with(|| self.cards.map(card_value).cmp(&other.cards.map(card_value)))
+    fn hand_type_with_jokers(&self) -> HandType {
+        let counts = self.cards.into_iter().filter(|c| *c != 'J').counts();
+        let highest_count_card = counts
+            .iter()
+            .max_by_key(|(_card, count)| **count)
+            .map(|(card, _count)| *card)
+            // If hand consists of just jokers, replace them with a valid label, like aces.
+            .unwrap_or('A');
+        let hand_with_jokers_replaced = Hand {
+            cards: self
+                .cards
+                .map(|c| if c == 'J' { highest_count_card } else { c }),
+        };
+        hand_with_jokers_replaced.hand_type()
     }
-}
 
-impl PartialOrd for Hand {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
+    fn cmp_with_jokers(&self, other: &Hand) -> Ordering {
+        self.hand_type_with_jokers()
+            .cmp(&other.hand_type_with_jokers())
+            .then_with(|| {
+                self.cards
+                    .map(card_value_with_jokers)
+                    .cmp(&other.cards.map(card_value_with_jokers))
+            })
     }
 }
 
 fn card_value(card_label: char) -> usize {
     [
         '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A',
+    ]
+    .into_iter()
+    .position(|l| l == card_label)
+    .unwrap_or(0)
+}
+
+fn card_value_with_jokers(card_label: char) -> usize {
+    [
+        'J', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A',
     ]
     .into_iter()
     .position(|l| l == card_label)
