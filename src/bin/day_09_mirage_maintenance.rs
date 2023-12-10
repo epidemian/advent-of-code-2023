204a0003295c667c2ff7a9d7a6cea0c6aa6f18b9
Demian Ferreiro <epidemian@gmail.com>
2023-12-10 01:46:23 -0300

    Add day 9 solution

---
+++ src/bin/day_09_mirage_maintenance.rs
@@ -0,0 +1,37 @@
+use itertools::Itertools;
+
+fn main() -> aoc::Result<()> {
+    let input = aoc::read_stdin()?;
+    let sequences: Vec<Vec<i64>> = input
+        .lines()
+        .map(|l| l.split(' ').map(str::parse).try_collect())
+        .try_collect()?;
+    let ans_1: i64 = sequences.iter().map(|s| extrapolate(s)).sum();
+    let ans_2: i64 = sequences.iter().map(|s| extrapolate_back(s)).sum();
+    println!("{ans_1} {ans_2}");
+    Ok(())
+}
+
+fn extrapolate(sequence: &[i64]) -> i64 {
+    if sequence.iter().all(|&n| n == 0) {
+        return 0;
+    }
+    let diffs = differences(sequence);
+    extrapolate(&diffs) + sequence[sequence.len() - 1]
+}
+
+fn extrapolate_back(sequence: &[i64]) -> i64 {
+    if sequence.iter().all(|&n| n == 0) {
+        return 0;
+    }
+    let diffs = differences(sequence);
+    sequence[0] - extrapolate_back(&diffs)
+}
+
+fn differences(sequence: &[i64]) -> Vec<i64> {
+    sequence
+        .iter()
+        .tuple_windows()
+        .map(|(a, b)| b - a)
+        .collect()
+}
