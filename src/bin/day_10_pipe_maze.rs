3ad8caa1ff8043f97dee0afac3923d20e7a5ba49
Demian Ferreiro <epidemian@gmail.com>
2023-12-11 03:31:19 -0300

    Small refactors for day 10

--- src/bin/day_10_pipe_maze.rs
+++ src/bin/day_10_pipe_maze.rs
@@ -1,48 +1,58 @@
+use itertools::Itertools;
+
 fn main() -> aoc::Result<()> {
     let input = aoc::read_stdin()?;
     let grid: Grid = input.lines().map(|line| line.chars().collect()).collect();
-    let height = grid.len();
-    let width = grid[0].len();
-    let start_pos = find_start_position(&grid).ok_or("start position not found")?;
-    let start_dir = [Up, Right, Down, Left]
-        .into_iter()
-        .find(|d| try_move(start_pos, *d, &grid).is_some())
-        .ok_or("no valid direction from start position found")?;
 
-    // Part 1
-    let mut is_loop_grid = vec![vec![false; width]; height];
-    let mut pos = start_pos;
-    let mut dir = start_dir;
-    let mut count = 0;
-    loop {
-        (pos, dir) = try_move(pos, dir, &grid)
-            .ok_or_else(|| format!("invalid turn, going {dir:?} from {pos:?}"))?;
-        count += 1;
-        is_loop_grid[pos.1 as usize][pos.0 as usize] = true;
-
-        if pos == start_pos {
-            break;
-        }
-    }
-    let half_pipe_length = count / 2;
-
-    // Part 2
-    let mut expanded_grid = expand_grid(&grid)?;
-    flood_fill(&mut expanded_grid);
-    let enclosed_count = count_enclosed_tiles(&grid, &expanded_grid);
+    let half_pipe_length = measure_pipe_loop(&grid)? / 2;
+    let enclosed_count = count_enclosed_tiles(&grid)?;
 
     println!("{half_pipe_length} {enclosed_count}");
     Ok(())
 }
 
-fn try_move((x, y): Point, dir: Dir, grid: &Grid) -> Option<(Point, Dir)> {
-    let new_pos = match dir {
-        Up => (x, y - 1),
-        Down => (x, y + 1),
-        Left => (x - 1, y),
-        Right => (x + 1, y),
+// Part 1 stuff
+type Grid = Vec<Vec<char>>;
+type Point = (usize, usize);
+
+#[derive(Debug, Copy, Clone)]
+enum Dir {
+    Up,
+    Right,
+    Down,
+    Left,
+}
+use Dir::*;
+
+fn measure_pipe_loop(grid: &Grid) -> aoc::Result<u32> {
+    let start_pos = find_start_position(grid).ok_or("start position not found")?;
+    let start_dir = [Up, Right, Down, Left]
+        .into_iter()
+        .find(|d| try_move(start_pos, *d, grid).is_some())
+        .ok_or("no valid direction from start position found")?;
+    let mut pos = start_pos;
+    let mut dir = start_dir;
+    let mut count = 0;
+    loop {
+        (pos, dir) = try_move(pos, dir, grid)
+            .ok_or_else(|| format!("invalid turn, going {dir:?} from {pos:?}"))?;
+        count += 1;
+        if pos == start_pos {
+            break;
+        }
+    }
+    Ok(count)
+}
+
+fn try_move(pos: Point, dir: Dir, grid: &Grid) -> Option<(Point, Dir)> {
+    let (mut x, mut y) = pos;
+    match dir {
+        Up => y = y.wrapping_sub(1),
+        Down => y += 1,
+        Left => x = x.wrapping_sub(1),
+        Right => x += 1,
     };
-    let ch = at(new_pos, grid)?;
+    let ch = grid.get(y)?.get(x)?;
     let new_dir = match (dir, ch) {
         (Left | Right, '-') => dir,
         (Up | Down, '|') => dir,
@@ -57,25 +67,7 @@ fn try_move((x, y): Point, dir: Dir, grid: &Grid) -> Option<(Point, Dir)> {
         (_, 'S') => dir,
         _ => return None,
     };
-    Some((new_pos, new_dir))
-}
-
-type Grid = Vec<Vec<char>>;
-type Point = (i32, i32);
-
-#[derive(Debug, Copy, Clone)]
-enum Dir {
-    Up,
-    Right,
-    Down,
-    Left,
-}
-use itertools::Itertools;
-use Dir::*;
-
-fn at((x, y): Point, grid: &Grid) -> Option<char> {
-    let ch = *grid.get(y as usize)?.get(x as usize)?;
-    Some(ch)
+    Some(((x, y), new_dir))
 }
 
 fn find_start_position(grid: &Grid) -> Option<Point> {
@@ -89,34 +81,104 @@ fn find_start_position(grid: &Grid) -> Option<Point> {
     None
 }
 
-fn expand_grid(grid: &Grid) -> aoc::Result<Vec<Vec<u8>>> {
+// Part 2 stuff
+type FloodFillGrid = Vec<Vec<FloodFillTile>>;
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+enum FloodFillTile {
+    Empty,
+    Wall,
+    Filled,
+}
+use FloodFillTile::*;
+
+fn count_enclosed_tiles(grid: &Grid) -> aoc::Result<u32> {
+    let mut expanded_grid = expand_grid(grid)?;
+    flood_fill(&mut expanded_grid);
+
+    let mut enclosed_count = 0;
+    for (y, row) in grid.iter().enumerate() {
+        for (x, _ch) in row.iter().enumerate() {
+            let is_enclosed = (0..3)
+                .cartesian_product(0..3)
+                .all(|(dx, dy)| expanded_grid[y * 3 + dy][x * 3 + dx] != Filled);
+            enclosed_count += is_enclosed as u32;
+        }
+    }
+    Ok(enclosed_count)
+}
+
+fn expand_grid(grid: &Grid) -> aoc::Result<FloodFillGrid> {
     let height = grid.len();
     let width = grid[0].len();
-    let mut expanded_grid = vec![vec![0; width * 3]; height * 3];
-    for (row, y) in grid.iter().zip(0..) {
-        for (ch, x) in row.iter().zip(0..) {
+    let mut expanded_grid = vec![vec![Empty; width * 3]; height * 3];
+    for (y, row) in grid.iter().enumerate() {
+        for (x, ch) in row.iter().enumerate() {
             let expanded_tile = expand_tile(ch)?;
-            for tile_y in 0..3 {
-                for tile_x in 0..3 {
-                    expanded_grid[y * 3 + tile_y][x * 3 + tile_x] = expanded_tile[tile_y][tile_x];
-                }
+            for (tx, ty) in (0..3).cartesian_product(0..3) {
+                expanded_grid[y * 3 + ty][x * 3 + tx] = expanded_tile[ty][tx];
             }
         }
     }
     Ok(expanded_grid)
 }
 
-const EMPTY: u8 = 0;
-const WALL: u8 = 1;
-const FILLED: u8 = 2;
+fn expand_tile(ch: &char) -> aoc::Result<[[FloodFillTile; 3]; 3]> {
+    #[allow(non_snake_case)]
+    let W = Wall;
+    let e = Empty;
+    Ok(match *ch {
+        '|' => [
+            [e, W, e], //
+            [e, W, e],
+            [e, W, e],
+        ],
+        '-' => [
+            [e, e, e], //
+            [W, W, W],
+            [e, e, e],
+        ],
+        'L' => [
+            [e, W, e], //
+            [e, W, W],
+            [e, e, e],
+        ],
+        'J' => [
+            [e, W, e], //
+            [W, W, e],
+            [e, e, e],
+        ],
+        '7' => [
+            [e, e, e], //
+            [W, W, e],
+            [e, W, e],
+        ],
+        'F' => [
+            [e, e, e], //
+            [e, W, W],
+            [e, W, e],
+        ],
+        '.' => [
+            [e, e, e], //
+            [e, e, e],
+            [e, e, e],
+        ],
+        'S' => [
+            [e, W, e], // Leave some gaps in the corner so the flood-fill can get in.
+            [W, W, W],
+            [e, W, e],
+        ],
+        _ => Err(format!("unexpected character {ch}"))?,
+    })
+}
 
-fn flood_fill(expanded_grid: &mut [Vec<u8>]) {
+fn flood_fill(expanded_grid: &mut FloodFillGrid) {
     let mut unvisited = vec![(0, 0)];
     while let Some((x, y)) = unvisited.pop() {
-        if expanded_grid[y][x] != EMPTY {
+        if expanded_grid[y][x] != Empty {
             continue;
         }
-        expanded_grid[y][x] = FILLED;
+        expanded_grid[y][x] = Filled;
         let neighbors = [
             (x, y.wrapping_sub(1)),
             (x, y + 1),
@@ -125,71 +187,9 @@ fn flood_fill(expanded_grid: &mut [Vec<u8>]) {
         ];
         for (nx, ny) in neighbors {
             let neighbor_tile = expanded_grid.get(ny).and_then(|row| row.get(nx));
-            if let Some(&EMPTY) = neighbor_tile {
+            if let Some(&Empty) = neighbor_tile {
                 unvisited.push((nx, ny))
             }
         }
     }
-}
-
-fn count_enclosed_tiles(grid: &Grid, expanded_grid: &[Vec<u8>]) -> u32 {
-    let mut enclosed_count = 0;
-    for (y, row) in grid.iter().enumerate() {
-        for (x, _ch) in row.iter().enumerate() {
-            let is_enclosed = (0..3)
-                .cartesian_product(0..3)
-                .all(|(dx, dy)| expanded_grid[y * 3 + dy][x * 3 + dx] != FILLED);
-            enclosed_count += is_enclosed as u32;
-        }
-    }
-    enclosed_count
-}
-
-#[allow(non_snake_case)]
-fn expand_tile(ch: &char) -> aoc::Result<[[u8; 3]; 3]> {
-    let W = WALL;
-    let O = EMPTY;
-    Ok(match *ch {
-        '|' => [
-            [O, W, O], //
-            [O, W, O],
-            [O, W, O],
-        ],
-        '-' => [
-            [O, O, O], //
-            [W, W, W],
-            [O, O, O],
-        ],
-        'L' => [
-            [O, W, O], //
-            [O, W, W],
-            [O, O, O],
-        ],
-        'J' => [
-            [O, W, O], //
-            [W, W, O],
-            [O, O, O],
-        ],
-        '7' => [
-            [O, O, O], //
-            [W, W, O],
-            [O, W, O],
-        ],
-        'F' => [
-            [O, O, O], //
-            [O, W, W],
-            [O, W, O],
-        ],
-        '.' => [
-            [O, O, O], //
-            [O, O, O],
-            [O, O, O],
-        ],
-        'S' => [
-            [O, W, O], // Leave some gaps in the corner so the flood-fill can get in.
-            [W, W, W],
-            [O, W, O],
-        ],
-        _ => Err(format!("unexpected character {ch}"))?,
-    })
 }
