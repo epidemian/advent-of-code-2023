f94b982e2100338fca5b70622df2de24df45af5d
Demian Ferreiro <epidemian@gmail.com>
2023-12-18 23:46:39 -0300

    Add debug print visualization for day 17 minimum path

--- src/bin/day_17_clumsy_crucible.rs
+++ src/bin/day_17_clumsy_crucible.rs
@@ -1,24 +1,25 @@
 use itertools::Itertools;
 use pathfinding::directed::dijkstra::dijkstra;
+use std::env;
 
 fn main() -> aoc::Result<()> {
     let input = aoc::read_stdin()?;
+    let parse_digit = |ch: char| ch.to_digit(10).ok_or("unexpected non-digit character");
     let city = input
         .lines()
-        .map(|l| {
-            l.chars()
-                .map(|ch| ch.to_digit(10).ok_or("unexpected non-digit character"))
-                .try_collect()
-        })
+        .map(|l| l.chars().map(parse_digit).try_collect())
         .try_collect()?;
+
     let min_heat_loss_p1 = find_min_heat_loss(&city, 0, 3)?;
     let min_heat_loss_p2 = find_min_heat_loss(&city, 4, 10)?;
     println!("{min_heat_loss_p1} {min_heat_loss_p2}");
     Ok(())
 }
 
+type Grid = Vec<Vec<u32>>;
+
 fn find_min_heat_loss(
-    city: &Vec<Vec<u32>>,
+    city: &Grid,
     min_straight_len: u32,
     max_straight_len: u32,
 ) -> aoc::Result<u32> {
@@ -26,7 +27,7 @@ fn find_min_heat_loss(
     let width = city[0].len();
     let start = (0_usize, 0_usize, Dir::None, 0);
     let goal = (width - 1, height - 1);
-    let (_path, min_heat_loss) = dijkstra(
+    let (path, min_heat_loss) = dijkstra(
         &start,
         |&(x, y, dir, straight_len)| {
             [
@@ -53,7 +54,9 @@ fn find_min_heat_loss(
         },
         |&(x, y, _d, straight_len)| (x, y) == goal && straight_len >= min_straight_len,
     )
-    .ok_or("couldn't find a path to the machine parts factory with ultra crucibles")?;
+    .ok_or("couldn't find a path to the machine parts factory")?;
+
+    debug_print_path(city, path);
 
     Ok(min_heat_loss)
 }
@@ -77,4 +80,29 @@ impl Dir {
             Dir::Left => Dir::Right,
         }
     }
+}
+
+fn debug_print_path(city: &Grid, path: Vec<(usize, usize, Dir, u32)>) {
+    if env::var("DEBUG").is_err() {
+        return;
+    }
+    for (y, row) in city.iter().enumerate() {
+        let mut line = String::new();
+        for (x, block) in row.iter().enumerate() {
+            if let Some((_, _, d, _)) = path.iter().find(|&&(px, py, ..)| (px, py) == (x, y)) {
+                let ch = match d {
+                    Dir::None => 'S',
+                    Dir::Up => '^',
+                    Dir::Right => '>',
+                    Dir::Down => 'v',
+                    Dir::Left => '<',
+                };
+                line.push_str(&format!("\x1b[34;1m{ch}\x1b[0m"));
+            } else {
+                line.push_str(&block.to_string());
+            }
+        }
+        println!("{line}");
+    }
+    println!();
 }
