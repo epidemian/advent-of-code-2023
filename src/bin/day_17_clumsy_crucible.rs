d0624df68d9b78cf08a1e22862dfe875a00a32c4
Demian Ferreiro <epidemian@gmail.com>
2023-12-18 23:23:38 -0300

    Day 17: extract common function for part 1 & 2

--- src/bin/day_17_clumsy_crucible.rs
+++ src/bin/day_17_clumsy_crucible.rs
@@ -3,7 +3,7 @@ use pathfinding::directed::dijkstra::dijkstra;
 
 fn main() -> aoc::Result<()> {
     let input = aoc::read_stdin()?;
-    let city: &Vec<Vec<u32>> = &input
+    let city = input
         .lines()
         .map(|l| {
             l.chars()
@@ -11,12 +11,22 @@ fn main() -> aoc::Result<()> {
                 .try_collect()
         })
         .try_collect()?;
+    let min_heat_loss_p1 = find_min_heat_loss(&city, 0, 3)?;
+    let min_heat_loss_p2 = find_min_heat_loss(&city, 4, 10)?;
+    println!("{min_heat_loss_p1} {min_heat_loss_p2}");
+    Ok(())
+}
+
+fn find_min_heat_loss(
+    city: &Vec<Vec<u32>>,
+    min_straight_len: u32,
+    max_straight_len: u32,
+) -> aoc::Result<u32> {
     let height = city.len();
     let width = city[0].len();
+    let start = (0_usize, 0_usize, Dir::None, 0);
     let goal = (width - 1, height - 1);
-    let start = (0usize, 0usize, Dir::None, 0);
-
-    let (_path, min_heat_loss_p1) = dijkstra(
+    let (_path, min_heat_loss) = dijkstra(
         &start,
         |&(x, y, dir, straight_len)| {
             [
@@ -31,49 +41,21 @@ fn main() -> aoc::Result<()> {
                 if nd == dir.opposite() {
                     return None;
                 }
+                if dir != Dir::None && nd != dir && straight_len < min_straight_len {
+                    return None;
+                }
                 let neighbor_straight_len = if nd == dir { straight_len + 1 } else { 1 };
-                if neighbor_straight_len >= 4 {
+                if neighbor_straight_len > max_straight_len {
                     return None;
                 }
                 Some(((nx, ny, nd, neighbor_straight_len), cost))
             })
         },
-        |&(x, y, ..)| (x, y) == goal,
-    )
-    .ok_or("couldn't find a path to the machine parts factory")?;
-
-    let (_path, min_heat_loss_p2) = dijkstra(
-        &start,
-        |&(x, y, dir, straight_len)| {
-            [
-                (x + 1, y, Dir::Right),
-                (x, y + 1, Dir::Down),
-                (x.wrapping_sub(1), y, Dir::Left),
-                (x, y.wrapping_sub(1), Dir::Up),
-            ]
-            .into_iter()
-            .filter_map(move |(nx, ny, nd)| {
-                let cost = *city.get(ny)?.get(nx)?;
-                if nd == dir.opposite() {
-                    return None;
-                }
-                if dir != Dir::None && nd != dir && straight_len < 4 {
-                    return None;
-                }
-                let neighbor_straight_len = if nd == dir { straight_len + 1 } else { 1 };
-                if neighbor_straight_len >= 11 {
-                    return None;
-                }
-                Some(((nx, ny, nd, neighbor_straight_len), cost))
-            })
-        },
-        |&(x, y, _d, straight_len)| (x, y) == goal && straight_len >= 4,
+        |&(x, y, _d, straight_len)| (x, y) == goal && straight_len >= min_straight_len,
     )
     .ok_or("couldn't find a path to the machine parts factory with ultra crucibles")?;
 
-    println!("{min_heat_loss_p1} {min_heat_loss_p2}");
-
-    Ok(())
+    Ok(min_heat_loss)
 }
 
 #[derive(Eq, PartialEq, Hash, Clone, Copy)]
