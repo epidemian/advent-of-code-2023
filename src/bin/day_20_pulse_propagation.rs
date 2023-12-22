333a76267d75d685fd21c6a08fb8da529c81cab8
Demian Ferreiro <epidemian@gmail.com>
2023-12-22 05:08:38 -0300

    Add solution for day 20 part 2

--- src/bin/day_20_pulse_propagation.rs
+++ src/bin/day_20_pulse_propagation.rs
@@ -8,29 +8,28 @@ fn main() -> aoc::Result<()> {
         .into_iter()
         .map(|(name, module, outputs)| ((name, module), (name, outputs)))
         .unzip();
+    let is_real_input = input.contains("-> rx");
 
-    // Connect conjunction modules with their inputs.
+    // Wire conjunction modules with their inputs.
     for (name, outs) in outputs.iter() {
         for out_name in outs.iter() {
             if let Some(Module::Conj { mem }) = modules.get_mut(*out_name) {
-                mem.insert(name.to_string(), false);
+                mem.insert(name, LOW);
             }
         }
     }
+
     let mut low_count = 0;
     let mut high_count = 0;
+    let mut conj_first_low_emits = HashMap::new();
 
-    for _ in 0..1000 {
+    for button_push in 1.. {
         let mut pulses = VecDeque::from_iter([("button", "broadcaster", LOW)]);
 
         while let Some(pulse) = pulses.pop_front() {
             let (src, dst, value) = pulse;
-            // println!("{src} -{value}-> {dst}");
-            if value {
-                high_count += 1;
-            } else {
-                low_count += 1;
-            }
+            high_count += value as u64;
+            low_count += !value as u64;
 
             let mut emit_pulse = |value| {
                 let new_pulse_src = dst;
@@ -53,14 +52,57 @@ fn main() -> aoc::Result<()> {
                     }
                 }
                 Module::Conj { mem } => {
-                    *mem.get_mut(src).unwrap() = value;
-                    let emit_value = !mem.values().all(|&v| v == HIGH);
-                    emit_pulse(emit_value);
+                    mem.insert(src, value);
+                    let emit_val = !mem.values().all(|&v| v == HIGH);
+                    emit_pulse(emit_val);
+                    if emit_val == LOW && !conj_first_low_emits.contains_key(dst) {
+                        conj_first_low_emits.insert(dst, button_push);
+                    }
                 }
             }
         }
+
+        if button_push == 1000 {
+            let part_1_ans = low_count * high_count;
+            if is_real_input {
+                print!("{part_1_ans} ");
+            } else {
+                println!("{part_1_ans}");
+                break;
+            }
+        }
+
+        // Note: multi-input conjunctions work like NANDs and single-input conjunctions, aka
+        // inverters, work like NOTs.
+        //
+        // This is *very* ad-hoc for our input module configuration. The "rx" module is fed by a
+        // single conjunction module that is in turn fed by a bunch of inverters that are fed by
+        // other conjunctions. So, the conjunction that feeds "rx" will emit a low pulse only when
+        // all these other conjunctions, in turn, emit a low pulse.
+        //
+        // We keep track of when these second-level conjunctions emit their first low pulse, and
+        // assume that this is their cycle time (because they are in turn fed by a bunch of
+        // flip-flops that repeat their cycles). So, we need to know when all these cycles coincide.
+        // Luckily, all these cycles are prime-numbers, so multiplying them together we get the
+        // super-cycle that makes them all emit low pulses at the same time, and in turn get the
+        // first low pulse into "rx".
+        //
+        // This solution definitely does *not* generalize to other module configurations.
+        if is_real_input {
+            let first_low_pulse_to_rx: Option<u64> = modules
+                .iter()
+                .filter(|&(name, module)| {
+                    matches!(module, Module::Conj { mem } if mem.len() > 1)
+                        && outputs[name] != ["rx"]
+                })
+                .map(|(name, _module)| conj_first_low_emits.get(name))
+                .product();
+            if let Some(part_2_ans) = first_low_pulse_to_rx {
+                println!("{part_2_ans:?}");
+                break;
+            }
+        }
     }
-    println!("{}", low_count * high_count);
 
     Ok(())
 }
@@ -68,11 +110,10 @@ fn main() -> aoc::Result<()> {
 const LOW: bool = false;
 const HIGH: bool = true;
 
-#[derive(Debug)]
-enum Module {
+enum Module<'a> {
     Broadcaster {},
     FlipFlop { state: bool },
-    Conj { mem: HashMap<String, bool> },
+    Conj { mem: HashMap<&'a str, bool> },
 }
 
 fn parse_modules(input: &str) -> aoc::Result<Vec<(&str, Module, Vec<&str>)>> {
