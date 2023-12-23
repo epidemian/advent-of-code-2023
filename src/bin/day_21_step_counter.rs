bab9e1d95bdd8b2a097baa4caa3527be5378193c
Demian Ferreiro <epidemian@gmail.com>
2023-12-23 19:27:45 -0300

    Add comments about assumptions made for day 21 part 2
    Also remove expected output for sample file, since it doesn't really
    make sense to check for something that the puzzle description does not
    tell us.

--- src/bin/day_21_step_counter.rs
+++ src/bin/day_21_step_counter.rs
@@ -4,51 +4,19 @@ use pathfinding::directed::dijkstra::dijkstra_reach;
 fn main() -> aoc::Result<()> {
     let input = aoc::read_stdin()?;
     let grid = input.lines().map(|l| l.chars().collect_vec()).collect_vec();
-    let size = grid.len() as i64;
-    let start = (size / 2, size / 2);
-    if grid[start.1 as usize][start.0 as usize] != 'S' {
-        Err("expected to start in the middle of the grid")?
-    }
+    let start = find_start_position(&grid).ok_or("starting position not found")?;
 
     let ans_1 = count_reachable_tiles(&grid, start, 64);
-
-    let part_2_step_count = 26501365;
-    assert_eq!(part_2_step_count % size, size / 2);
-
-    // For part 2, we do a quadratic extrapolation of this function.
-    // TODO: explain why we're assuming that this is quadratic.
-    // f(x) = ax² + bx + c
-    let f = |x: i64| count_reachable_tiles(&grid, start, (size / 2 + size * x) as u64) as i64;
-    let f_0 = f(0);
-    let f_1 = f(1);
-    let f_2 = f(2);
-
-    // Find coefficients a, b, c
-    // f(0) = c
-    // f(1) = a + b + c
-    // f(2) = 4a + 2b + c
-    // c = f(0)
-    let c = f_0;
-    // f(2) - 2*f(1) = 2a - c
-    // a = (f(2) - 2*f(1) + c) / 2
-    let a = (f_2 - 2 * f_1 + c) / 2;
-    // b = f(1) - a - c
-    let b = f_1 - a - c;
-    let f_quadratic = |x: i64| a * x * x + b * x + c;
-
-    assert_eq!(f_0, f_quadratic(0));
-    assert_eq!(f_1, f_quadratic(1));
-    assert_eq!(f_2, f_quadratic(2));
-
-    let ans_2 = f_quadratic(part_2_step_count / size);
+    let ans_2 = extrapolate_reachable_tiles(&grid, start, 26501365);
     println!("{ans_1} {ans_2}");
 
     Ok(())
 }
 
 type Grid = Vec<Vec<char>>;
+type Point = (i64, i64);
 
-fn count_reachable_tiles(grid: &Grid, start_pos: (i64, i64), steps_count: u64) -> u64 {
+fn count_reachable_tiles(grid: &Grid, start_pos: Point, steps_count: u64) -> u64 {
     let size = grid.len() as i64;
     let reachable_tiles = dijkstra_reach(&start_pos, |&(x, y), cost| {
         let dirs = [(0, -1), (1, 0), (0, 1), (-1, 0)];
@@ -65,8 +33,60 @@ fn count_reachable_tiles(grid: &Grid, start_pos: (i64, i64), steps_count: u64) -
         })
     });
     let evenness = steps_count % 2;
-    let reachable_tiles_count = reachable_tiles
+    reachable_tiles
         .filter(|item| item.total_cost % 2 == evenness)
-        .count() as u64;
-    reachable_tiles_count
+        .count() as u64
+}
+
+fn extrapolate_reachable_tiles(grid: &Grid, start: Point, steps_count: i64) -> i64 {
+    let size = grid.len() as i64;
+
+    // For part 2, we do a quadratic extrapolation of this function.
+    let f = |x: i64| count_reachable_tiles(grid, start, (size / 2 + size * x) as u64) as i64;
+
+    // In case of the actual input, this function is:
+    // f(x) = #reachable_tiles(x * 131 + 65)
+    //
+    // And we're assuming this behaves quadratically because the input map has the start position in
+    // the middle and allows the elf to walk to any of its border tiles in a "straight line" (i.e.,
+    // moving straight left/right and then up/down. This is because all tiles up, down, left or
+    // right from the start are clear, as well as all the tiles in the border. So all of the grid
+    // copies in the infinite 2D plane will be reachable for walking in the same way.
+    //
+    // If f is quadratic, it can be expressed as:
+    // f(x) = ax² + bx + c
+    // So we need to find the coefficients a, b, c. We can do so by evaluating the function a couple
+    // of times and getting the coefficients from there:
+    // f(0) = c
+    // f(1) = a + b + c
+    // f(2) = 4a + 2b + c
+    let f_0 = f(0);
+    let f_1 = f(1);
+    let f_2 = f(2);
+    // c = f(0)
+    let c = f_0;
+    // f(2) - 2*f(1) = 2a - c
+    // a = (f(2) - 2*f(1) + c) / 2
+    let a = (f_2 - 2 * f_1 + c) / 2;
+    // b = f(1) - a - c
+    let b = f_1 - a - c;
+
+    let f_quadratic = |x| a * x * x + b * x + c;
+
+    assert_eq!(f_0, f_quadratic(0));
+    assert_eq!(f_1, f_quadratic(1));
+    assert_eq!(f_2, f_quadratic(2));
+
+    f_quadratic(steps_count / size)
+}
+
+fn find_start_position(grid: &Grid) -> Option<Point> {
+    for (row, y) in grid.iter().zip(0..) {
+        for (&tile, x) in row.iter().zip(0..) {
+            if tile == 'S' {
+                return Some((x, y));
+            }
+        }
+    }
+    None
 }
