174f6a6c8488ea8f4f7c825dc5111e47fd51a623
Demian Ferreiro <epidemian@gmail.com>
2024-01-02 20:56:18 -0300

    Add day 22 part 2 solution

--- src/bin/day_22_sand_slabs.rs
+++ src/bin/day_22_sand_slabs.rs
@@ -1,5 +1,5 @@
 use itertools::Itertools;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 fn main() -> aoc::Result<()> {
     let input = aoc::read_stdin()?;
@@ -8,6 +8,7 @@ fn main() -> aoc::Result<()> {
 
     let mut grid = HashMap::new();
     let mut supports = vec![vec![]; bricks.len()];
+    let mut supported_by = vec![vec![]; bricks.len()];
 
     for (brick_id, brick) in bricks.iter_mut().enumerate() {
         loop {
@@ -24,6 +25,9 @@ fn main() -> aoc::Result<()> {
                 if let Some(&other_brick_id) = grid.get(&new_point) {
                     if !supports[other_brick_id].contains(&brick_id) {
                         supports[other_brick_id].push(brick_id);
+                    }
+                    if !supported_by[brick_id].contains(&other_brick_id) {
+                        supported_by[brick_id].push(other_brick_id);
                     }
                     collides = true;
                 };
@@ -43,19 +47,25 @@ fn main() -> aoc::Result<()> {
         .iter()
         .enumerate()
         .filter(|&(brick_id, supported_bricks)| {
-            supported_bricks.iter().all(|supported_brick_id| {
-                supports
+            supported_bricks.iter().all(|&supported_brick_id| {
+                supported_by[supported_brick_id]
                     .iter()
-                    .enumerate()
-                    .any(|(other_brick_id, other_supported_bricks)| {
-                        other_brick_id != brick_id
-                            && other_supported_bricks.contains(supported_brick_id)
-                    })
+                    .any(|&other_brick_id| other_brick_id != brick_id)
             })
         })
         .count();
 
-    println!("{safe_disintegration_count}");
+    let total_fall_sum: usize = supports
+        .iter()
+        .enumerate()
+        .map(|(brick_id, _supported_bricks)| {
+            let mut falling_bricks = HashSet::from_iter([brick_id]);
+            count_falls_if_disintegrated(brick_id, &supports, &supported_by, &mut falling_bricks);
+            falling_bricks.len() - 1
+        })
+        .sum();
+
+    println!("{safe_disintegration_count} {total_fall_sum}");
 
     Ok(())
 }
@@ -76,6 +86,28 @@ fn brick_points(((x1, y1, z1), (x2, y2, z2)): Brick) -> Box<dyn Iterator<Item = 
         return Box::new((y1..=y2).map(move |y| (x1, y, z1)));
     }
     Box::new((z1..=z2).map(move |z| (x1, y1, z)))
+}
+
+fn count_falls_if_disintegrated(
+    brick_id: usize,
+    supports: &[Vec<usize>],
+    supported_by: &[Vec<usize>],
+    falling_bricks: &mut HashSet<usize>,
+) {
+    let unsupported_bricks: Vec<_> = supports[brick_id]
+        .iter()
+        .copied()
+        .filter(|&supported_brick_id| {
+            !supported_by[supported_brick_id]
+                .iter()
+                .any(|other_brick_id| !falling_bricks.contains(other_brick_id))
+        })
+        .collect();
+
+    falling_bricks.extend(unsupported_bricks.iter());
+    for &unsupported_brick_id in unsupported_bricks.iter() {
+        count_falls_if_disintegrated(unsupported_brick_id, supports, supported_by, falling_bricks)
+    }
 }
 
 fn parse_brick(line: &str) -> aoc::Result<Brick> {
