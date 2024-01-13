580213e5c187a0398b80b44f635e5a88003396c4
Demian Ferreiro <epidemian@gmail.com>
2024-01-13 18:07:34 -0300

    Day 25: Use a priority queue to reduce runtime
    This brings down runtime from ~3s to ~500ms

--- src/bin/day_25_snowverload.rs
+++ src/bin/day_25_snowverload.rs
@@ -1,12 +1,12 @@
 use anyhow::ensure;
 use itertools::Itertools;
-use std::collections::HashMap;
+use priority_queue::PriorityQueue;
+use std::collections::{HashMap, HashSet};
 
 fn main() -> aoc::Result<()> {
     let input = aoc::read_stdin()?;
     let graph = parse_graph(&input);
-    let mat = create_adjacency_matrix(&graph);
-    let (cut_size, cut_nodes) = global_min_cut(mat);
+    let (cut_size, cut_nodes) = global_min_cut(to_index_graph(&graph));
     ensure!(
         cut_size == 3,
         "expected the minimum cut of 3, got {cut_size}"
@@ -17,7 +17,7 @@ fn main() -> aoc::Result<()> {
 }
 
 fn parse_graph(input: &str) -> HashMap<&str, Vec<&str>> {
-    let mut graph: HashMap<&str, Vec<&str>> = HashMap::new();
+    let mut graph: HashMap<_, Vec<_>> = HashMap::new();
     for line in input.lines() {
         let node_a = &line[0..3];
         for node_b in line[5..].split_whitespace() {
@@ -28,55 +28,66 @@ fn parse_graph(input: &str) -> HashMap<&str, Vec<&str>> {
     graph
 }
 
-fn create_adjacency_matrix(graph: &HashMap<&str, Vec<&str>>) -> Vec<Vec<i64>> {
-    let node_indices: HashMap<_, _> = graph.keys().zip(0..).collect();
-    let mut mat = vec![vec![0; graph.len()]; graph.len()];
-    for (node, adjacent_nodes) in graph {
-        let i = node_indices[node];
-        for adj_node in adjacent_nodes.iter() {
-            let j = node_indices[adj_node];
-            mat[i][j] = 1;
-            mat[j][i] = 1;
-        }
+fn to_index_graph(graph: &HashMap<&str, Vec<&str>>) -> Vec<HashSet<usize>> {
+    let node_indices: HashMap<_, _> = graph.keys().sorted().zip(0..).collect();
+    let mut index_graph = vec![HashSet::new(); graph.len()];
+    for (n, adj) in graph.iter() {
+        index_graph[node_indices[&n]] = adj.iter().map(|m| node_indices[m]).collect();
     }
-    mat
+    index_graph
 }
 
 // Computes the minimum cut of a graph using the Stoer–Wagner algorithm. The graph is given as an
 // adjacency matrix.
 // See: https://en.wikipedia.org/wiki/Stoer%E2%80%93Wagner_algorithm
-// This implementation is just a translation of this C++ code: https://github.com/kth-competitive-programming/kactl/blob/782a5f4e38fff0efb2ae83761e18fb829d6aa00c/content/graph/GlobalMinCut.h
-#[allow(clippy::needless_range_loop)]
-fn global_min_cut(mut mat: Vec<Vec<i64>>) -> (i64, Vec<usize>) {
-    let n = mat.len();
+// This implementation is based on this C++ code: https://github.com/kth-competitive-programming/kactl/blob/782a5f4e38fff0efb2ae83761e18fb829d6aa00c/content/graph/GlobalMinCut.h
+fn global_min_cut(mut graph: Vec<HashSet<usize>>) -> (i64, Vec<usize>) {
+    let n = graph.len();
     let mut best = (i64::MAX, vec![]);
     let mut co = (0..n).map(|i| vec![i]).collect_vec();
+    let mut nodes = (0..n).collect_vec();
+    let mut mat = vec![vec![0; n]; n];
+    for (n, adj) in graph.iter().enumerate() {
+        for &m in adj.iter() {
+            mat[n][m] = 1;
+            mat[m][n] = 1;
+        }
+    }
 
-    for phase in 1..n {
-        let mut w = mat[0].clone();
-        let mut s = 0;
-        let mut t = 0;
-        for _ in 0..n - phase {
-            w[t] = i64::MIN;
+    while nodes.len() > 1 {
+        let a = nodes[0];
+        let mut q: PriorityQueue<_, _> = graph[a].iter().map(|&n| (n, mat[a][n])).collect();
+        let mut s = a;
+        let mut t = a;
+        let mut cut = 0;
+        for _ in 0..nodes.len() - 1 {
+            q.push_decrease(t, i64::MIN);
             s = t;
-            t = w.iter().position_max().unwrap();
-            for i in 0..n {
-                w[i] += mat[t][i]
+            (t, cut) = q.pop().expect("queue should contain a node");
+            for &n in &graph[t] {
+                if !q.change_priority_by(&n, |w| *w += mat[t][n]) {
+                    q.push(n, mat[t][n]);
+                }
             }
         }
-        let min_cut_of_phase = w[t] - mat[t][t];
+        let min_cut_of_phase = cut;
         if min_cut_of_phase < best.0 {
             best = (min_cut_of_phase, co[t].clone());
         }
         let co_t = co[t].clone();
         co[s].extend(co_t);
-        for i in 0..n {
-            mat[s][i] += mat[t][i]
+
+        for n in graph[t].clone() {
+            graph[n].remove(&t);
+            if n != s {
+                graph[s].insert(n);
+                graph[n].insert(s);
+                mat[s][n] += mat[t][n];
+                mat[n][s] = mat[s][n];
+            }
         }
-        for i in 0..n {
-            mat[i][s] = mat[s][i]
-        }
-        mat[0][t] = i64::MIN;
+        nodes.remove(nodes.iter().position(|&n| n == t).unwrap());
+        graph[t].clear();
     }
 
     best
