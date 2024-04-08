e434bdbd835143b8c2b8ca3f7c06d5d71dcc3ba1
Demian Ferreiro <epidemian@gmail.com>
2024-04-07 22:07:49 -0300

    Day 3: Simplify number-finding logic even further
    Delegate to slice::chunk_by() for finding the spans of consecutive
    digits.

--- src/bin/day_03_gear_ratios.rs
+++ src/bin/day_03_gear_ratios.rs
@@ -4,7 +4,7 @@ use std::collections::HashMap;
 fn main() -> aoc::Result<()> {
     let input = aoc::read_stdin()?;
     let (grid, width, height) = aoc::parse_char_grid(&input)?;
-    let number_spans = get_number_spans(&grid)?;
+    let number_spans = find_numbers(&grid)?;
 
     // Part 1
     let part_numbers_sum: u32 = number_spans
@@ -42,18 +42,15 @@ fn is_part_number(start_x: usize, end_x: usize, y: usize, grid: &Grid) -> bool {
 
 // Finds all numbers on the grid and returns tuples with (number, start_x, end_x, y) where
 // start_x..end_x is the span of the number on the grid.
-fn get_number_spans(grid: &Grid) -> aoc::Result<Vec<(u32, usize, usize, usize)>> {
+fn find_numbers(grid: &Grid) -> aoc::Result<Vec<(u32, usize, usize, usize)>> {
     let mut spans = vec![];
     for (y, line) in grid.iter().enumerate() {
-        let mut x = 0;
-        while x < line.len() {
-            let digits = grid[y][x..].iter().take_while(|ch| ch.is_ascii_digit());
-            let s = String::from_iter(digits);
-            if !s.is_empty() {
-                let num = s.parse()?;
-                spans.push((num, x, x + s.len(), y));
-            }
-            x += s.len().max(1)
+        let digit_indices = line.iter().positions(char::is_ascii_digit).collect_vec();
+        for chunk in digit_indices.chunk_by(|&i, &j| i == j - 1) {
+            let start_x = chunk[0];
+            let end_x = start_x + chunk.len();
+            let num = String::from_iter(&line[start_x..end_x]).parse()?;
+            spans.push((num, start_x, end_x, y))
         }
     }
     Ok(spans)
